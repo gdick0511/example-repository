3c408705f34374082d3987498d424c2c42381557
garrettdick <garrettd0511@gmail.com>
2021-09-16 15:53:50 -0500

    Made changes to CSS, HTML and added Event Listeners via JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This energy is brought to you by coffee";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("div").append(h2);
+
+const theBox = document.getElementById("myBox")
+theBox.addEventListener('click', function(){
+    alert('I was clicked!');
+});
+
+// const theBox = document.getElementById("myBox")
+// theBox.addEventListener('click', function(){
+//     alert('I was clicked!');
+// });
+
+// myBox.addEventListener('click', function() {
+//     alert('I was Checked!');
+//     });
+
+// function addingEventListener() {
+//     const myBox = document.getElementById("myBox");
+//     function clickAlert() {
+//         alert("I was checked!")
+//     }
+//     myBox.addEventListener('click', clickAlert);
+// }
+
+// addingEventListener(); clickAlert;
+
+
